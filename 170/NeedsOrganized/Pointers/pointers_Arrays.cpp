cae19439f2d0972c21e9693f44c87edef6d5b287
badair <badair@harding.edu>
2013-01-10 21:42:26 -0600

    changed names and moved
    also restored some content into pointers_Arrays.cpp

--- 170/NeedsOrganized/Pointers/pointers_Arrays.cpp
+++ 170/NeedsOrganized/Pointers/pointers_Arrays.cpp
@@ -18,7 +18,29 @@ int main()
 	cout << p << endl;   // outputs p, which is the address of x
 	cout << *p << endl;  // outputs "what is stored at address p" (the integert 9)
    
-   
+  	int A[10]; // indexes 0-9 - 10 ints
+
+	for(int i = 0; i < 10; i++)
+	{
+		A[i] = i * 5;
+		//*(A + i) = i * 5;
+	}
+
+	for(int i = 0; i < 10; i++)
+	{
+		cout << A[i] << " ";
+	}
+
+	p = A;
+
+	cout << endl;
+
+	while( p < (A + 10))
+	{
+		cout << *p << " ";
+		p++;
+	}
+
    cin.get();
    return 0;
 }
